04a088938b045287fa51770a8478a761a1285dac
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-27 10:16:39 -0300

    Cria o modelo de empresa

---
+++ src/models/Company.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const CompanyModel = new mongoose.Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    legalName: {
+      type: String,
+      required: [true, "Razão Social é um campo obrigatório."],
+      unique: true,
+    },
+    tradeName: {
+      type: String,
+    },
+    documentNumber: {
+      type: String,
+      required: [true, "CNPJ é um campo obrigatório."],
+      unique: true,
+    },
+    cnaeCode: {
+      type: String,
+      required: [true, "CNAE é um campo obrigatório."],
+    },
+    address: {
+      type: String,
+    },
+    contactPhone: {
+      type: Number,
+      required: [true, "Telefone para contato é um campo obrigatório."],
+    },
+    contactEmail: {
+      type: String,
+    },
+    creditScore: {
+      Type: Number,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+export default CompanyModel;
